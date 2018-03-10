92cf4883c27100dfe034c4063468128393d875ad
Mateusz Gołos <golosmateusz@gmail.com>
2018-03-10 16:30:58 +0100

    .

---
+++ warsztaty/javascript/podstawy/js/funkcje.js
@@ -0,0 +1,16 @@
+// funkcja bez parametrów
+function wyswietlWKonsoli() {
+    console.log("Mateusz Gołos");
+
+    // wyswietlWKonsoli():
+
+    // funkcje z parametrami 
+
+    function wyswietlWKonsoliParam(imie, nazwisko) {
+        console.log(imie + " " + nazwisko);
+    }
+
+    wyswietlWKonsoliParam("Mateusz", "Gołos");
+    wyswietlWKonsoliParam("Adam", "Nowacki");
+    wyswietlWKonsoliParam("Kinga", "Korycka");
+    wyswietlWKonsoliParam("Adrian", "Bąk");
--- warsztaty/javascript/zmienne/js/zmienne.js
+++
@@ -1,9 +0,0 @@
-var imie;
-var nazwisko = "Gołos";
-var wiek = 26;
-
-imie = "Mateusz";
-
-// łączenie zmiennych
-var laczenieTekstu = "Moje imie to: " + imie + " a nazwisko to " + nazwisko;
-console.log(laczenieTekstu);
