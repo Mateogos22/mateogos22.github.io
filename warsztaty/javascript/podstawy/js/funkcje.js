1b9d76c917964f958967c142610e2408165a07e7
Mateusz Gołos <golosmateusz@gmail.com>
2018-03-10 16:43:00 +0100

    podstawy

--- warsztaty/javascript/podstawy/js/funkcje.js
+++ warsztaty/javascript/podstawy/js/funkcje.js
@@ -1,16 +1,26 @@
+
 // funkcja bez parametrów
 function wyswietlWKonsoli() {
     console.log("Mateusz Gołos");
-
+}
     // wyswietlWKonsoli():
 
     // funkcje z parametrami 
 
-    function wyswietlWKonsoliParam(imie, nazwisko) {
+    function wyswietlWKonsoliParam (imie, nazwisko) {
         console.log(imie + " " + nazwisko);
     }
 
     wyswietlWKonsoliParam("Mateusz", "Gołos");
     wyswietlWKonsoliParam("Adam", "Nowacki");
     wyswietlWKonsoliParam("Kinga", "Korycka");
-    wyswietlWKonsoliParam("Adrian", "Bąk");
+
+
+    // zwracanie wartości przez funkcję 
+    function obliczPodatek( kwota, procentPodatek) {
+        var wynik = kwota * (procentPodatek / 100);
+        return wynik.toFixed(2);
+    }
+    
+    var obliczenia1 = obliczPodatek( 4990, 23 );
+    console.log( "Wysokość obliczonego podatku wynosi: " + obliczenia1 );
