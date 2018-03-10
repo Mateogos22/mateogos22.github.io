5d39d7676d2443480377d255ac5fb8ae8542b972
Mateusz Gołos <golosmateusz@gmail.com>
2018-03-10 16:06:36 +0100

    zmienne

---
+++ warsztaty/javascript/zmienne/js/zmienne.js
@@ -0,0 +1,9 @@
+var imie;
+var nazwisko = "Gołos";
+var wiek = 26;
+
+imie = "Mateusz";
+
+// łączenie zmiennych
+var laczenieTekstu = "Moje imie to: " + imie + " a nazwisko to " + nazwisko;
+console.log(laczenieTekstu);
