553789cc7dfd4dce28b19d0e12a05f18a58c7ef0
Mateusz Gołos <golosmateusz@gmail.com>
2018-03-11 16:16:15 +0100

    pętle

---
+++ warsztaty/javascript/podstawy/js/petle.js
@@ -0,0 +1,46 @@
+//pętla "for"
+//for( var i = 1; i <= 10; i++ ) {
+//    console.log( "Iteracja nr " + i );
+//}
+
+//tablica
+
+var tablica = ["Volvo", "Mazda", "Honda", "Audi"];
+//
+//for( var i = 0; 1 < tablica.length; i++ ) {
+//    console.log( tablica[i] );
+//}
+
+//pętla forEach
+
+//tablica.forEach( function(element, index)  {
+//                console.log("Index: " + index + " Element: " + element);
+//} );
+
+//pętle while
+
+//var iter = 1;
+////while( iter <= 25 ) {
+////    console.log( "White iteracja nr " + iter );
+////    iter++;
+////}
+//
+//do {
+//    console.log( "White iteracja nr " + iter );
+//} while( iter >= 25)
+//
+
+//break i continue
+
+for( var i = 1; i < 25; i++ ) {
+    if( i > 6 ) {
+        break;
+    } else {
+        console.log( "Obrót pętli nr " + i );
+    }
+}
+
+
+
+
+
