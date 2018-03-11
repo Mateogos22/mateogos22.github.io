0d1ae2040dd1ca9f7c014ad15ee8b82a5a86272b
Mateusz Gołos <golosmateusz@gmail.com>
2018-03-11 15:22:14 +0100

    if, else if, swich

---
+++ warsztaty/javascript/podstawy/js/instrukcje_warunkowe.js
@@ -0,0 +1,75 @@
+//instrukcje warunkowe
+
+var zmienna1 = "pies";
+var zmienna2 = "kot";
+
+//if( zmienna1 == zmienna2 ) {
+//    console.log("Spełniam warunek");
+//}
+
+//instrukcja if else
+//if( zmienna1 == zmienna2 ) {
+//    console.log("Spełniam warunek");
+//} else {
+//    console.log("Nie spełniam warunku");
+//}
+
+
+//instrukcja else if
+
+//var mojaLiczba = 44;
+//
+//if( mojaLiczba == 5 ) {
+//    console.log("Moja liczba to 5");
+//} else if (mojaLiczba == 12) {
+//    console.log("Moja liczba to 12");
+//} else if( mojaLiczba == 44 ) {
+//    console.log("Moja liczba to 44");
+//} else {
+//    console.log("Moja liczna ma inną wartość");
+//}
+
+
+//switch
+var kolor = "żółty";
+
+switch (kolor) {
+    case "zielony":
+        console.log("kolor zielony");
+    break;
+    case "czerwony":
+        console.log("kolor czerwony");
+    break;
+    default : 
+        console.log("Nie znalazłem dopasowania koloru");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
