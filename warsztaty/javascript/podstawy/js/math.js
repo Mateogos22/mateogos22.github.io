2929993b5f6b5ff4b842bc5c257c61b7b6dba2d3
Mateusz Gołos <golosmateusz@gmail.com>
2018-03-11 14:01:31 +0100

    math

---
+++ warsztaty/javascript/podstawy/js/math.js
@@ -0,0 +1,9 @@
+// właściwości obiektu Math
+console.log( Math.E );
+console.log( Math.PI );
+console.log( Math.SQRT2 );
+
+//metody obiektu
+console.log(Math.cos(30));
+console.log(Math.random()*10);
+console.log(Math.pow(2,7));
