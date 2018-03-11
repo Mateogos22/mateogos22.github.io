6b1a05382163c61e32644b7e55fe22d1a5af81fb
Mateusz Gołos <golosmateusz@gmail.com>
2018-03-11 14:48:50 +0100

    operatory

---
+++ warsztaty/javascript/podstawy/js/operatory.js
@@ -0,0 +1,101 @@
+////operatory arytmetyczne
+//var liczba1 = 10;
+//var liczba2 = 5;
+//
+////dodawanie
+//var wynik = liczba1 + liczba2;
+//
+//console.log( wynik );
+//
+////odejmowanie
+//wynik = liczba1 - liczba2;
+//
+//console.log( wynik );
+//
+////mnożenie
+//wynik = liczba1 * liczba2;
+//
+//console.log( wynik );
+//
+////dzielenie 
+//wynik = liczba1 / liczba2;
+//
+//console.log( wynik );
+//
+////modulo
+//wynik = liczba1 % liczba2;
+//
+//console.log( wynik );
+//
+////inkrementacja
+//liczba2++;
+//
+//console.log( liczba2 );
+//
+////dekrementacja
+//liczba2--;
+//
+//console.log( liczba2 );
+//
+
+
+
+
+/*
+    operatory przypisania
+*/
+
+//var liczba3 = 200;
+//var liczba4 = 100;
+//var liczba5 = 15;
+//var liczba6 = 40;
+//var libcza7 = 7;
+//
+//
+//liczba3 += 200; // liczba3 = liczba3 + 200;
+//liczba4 -= 50;  // liczba4 = liczba4 - 50;
+//liczba5 *= 2;  // liczba5 = liczba5 * 2;
+//liczba6 /= 2;  // liczba6 = liczba6 / 2;
+//libcza7 %= 2; // liczba7 = liczba7 % 2;
+//
+//console.log( libcza7 );
+
+/*
+    operatory porównania
+*/
+
+//var liczba8 = 10;
+//var liczba9 = 10;
+//
+//if( liczba8 <= liczba9 ) {
+//    console.log( "Warunek jest spełniony" );
+//} else {
+//    console.log( "Warunek nie jest spełniony" );
+//}
+
+/*
+    operatory logiczne
+*/
+
+var liczba10 = 20;
+var liczba11 = 4;
+var czyWiosna = "tak";
+
+if( !czyWiosna ) {
+    console.log( "Warunek jest spełniony" );
+} else {
+    console.log( "Warunek nie spełniony" );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
