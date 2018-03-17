2df5486d6124837a0250927f3643245963f025aa
Mateusz Gołos <golosmateusz@gmail.com>
2018-03-17 10:26:56 +0100

    model DOM podstawy

---
+++ warsztaty/dom/js/model-dom.js
@@ -0,0 +1,11 @@
+//console.log( document );
+//console.dir( document );
+
+// zwróci element body jako dokument
+//console.dir( document.body );
+
+//obiekt window
+//console.log( window );
+console.log( window.innerWidth );
+console.log( window.innerHeight );
+console.log( window.document );
