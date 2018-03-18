c890c4bef0a89572c56d7f518b3f98067c940229
Mateusz Gołos <golosmateusz@gmail.com>
2018-03-18 11:36:59 +0100

    jquery - document.ready

---
+++ warsztaty/jquery/js/main.js
@@ -0,0 +1,8 @@
+
+//document.ready gwaratnuje, że skrypty się wywołają w momencie gdy cały DOM jest już załadowany
+$(function(){
+   
+//    $(".title").css({color : "red", backgroundColor : "yellow"});
+    $("[class='title']").css({color: "red", backgroundColor: "yellow"});
+    
+});
