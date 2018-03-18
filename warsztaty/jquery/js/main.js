95549c2e889e0ec2b879e3ef32bbd84d470db683
Mateusz Gołos <golosmateusz@gmail.com>
2018-03-18 12:05:25 +0100

    jquery selektory

--- warsztaty/jquery/js/main.js
+++ warsztaty/jquery/js/main.js
@@ -1,8 +1,10 @@
 
 //document.ready gwaratnuje, że skrypty się wywołają w momencie gdy cały DOM jest już załadowany
+// poniżej skrócony zapis document.ready
 $(function(){
    
 //    $(".title").css({color : "red", backgroundColor : "yellow"});
     $("[class='title']").css({color: "red", backgroundColor: "yellow"});
     
 });
+
---
+++ warsztaty/jquery/js/selektory.js
@@ -0,0 +1,18 @@
+$(function(){
+   
+//     wybieranie po tagu HTML
+    var wszystkieParagrafy = $("p");
+//    console.log( wszystkieParagrafy );
+    
+//      wybieranie kilku selektorów
+    var mojeSelektory = $("h1, h2, .paragraf");
+//    console.log( mojeSelektory );
+    
+//    wybieranie po id
+    var wybierzId = $("#first");
+//    console.log( wybierzId );
+    
+    var pClass = $("div .paragraf");
+    console.log( pClass );
+    
+});
