ec03c1f88cd425338ffbb91aaab064d348166f0d
Mateusz Gołos <golosmateusz@gmail.com>
2018-03-18 17:02:55 +0100

    anim

---
+++ warsztaty/jquery/animacja/js/animation.js
@@ -0,0 +1,10 @@
+$(function(){
+    
+    var slideShow = $(".slide-show");
+    var slideCount = $("single-slide").length;
+    var slideWidth = 100/slideCount.length;
+    var slideIndex = 0;    
+    
+});
+
+$("slideShow").css({width: "400%"});
