bc3e9ff0aecc33814af400d4178a8d90bbc79991
Mateusz Gołos <golosmateusz@gmail.com>
2018-03-18 11:02:24 +0100

    war

--- warsztaty/dom/js/formularz.js
+++ warsztaty/dom/js/formularz.js
@@ -1,8 +1,8 @@
-function pobierzImie( event ){
-    var form = document.getElementsByTagName("input");
-    var imie = form[0].value;
-    var nazwisko = form[1].value;
-    event.preventDefault();
-    console.log( "Imię: " + imie + "\nNazwisko: " + nazwisko );
-}
+//function pobierzImie( event ){
+//    var form = document.getElementsByTagName("input");
+//    var imie = form[0].value;
+//    var nazwisko = form[1].value;
+//    event.preventDefault();
+//    console.log( "Imię: " + imie + "\nNazwisko: " + nazwisko );
+//}
 
