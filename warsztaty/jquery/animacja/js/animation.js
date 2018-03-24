e0e63e730aa464779898e75bbc5843ed23c51d02
Mateusz Gołos <golosmateusz@gmail.com>
2018-03-24 16:05:48 +0100

    animacja działa, poprawić wysokość

--- warsztaty/jquery/animacja/js/animation.js
+++ warsztaty/jquery/animacja/js/animation.js
@@ -1,22 +1,47 @@
 $(function(){
+    var slideShow = $('.slide-show');
+    var slideCount = $('.single-slide').length;
+    var slideWidth = 100/slideCount;
+    var slideIndex = 0;
     
-    var slideShow = $(".slide-show");
-    var slideCount = $("single-slide").length;
-    var slideWidth = 100/slideCount.length;
-    var slideIndex = 0;    
+//    szerokość "kliszy"
     
-});
-
-// szerokość kontenera 
-$("slideShow").css("width", slideCount + "+00%");
-
-slideShow.find("single-slide").each(function(index){
-    $(this).css("slideWidth" + "100%", "margin-left": "100%" )});
-
-slideShow.each(function( index ){
-    ( index )
+    slideShow.css('width', slideCount*100 + '%');
+    
+    slideShow.find('.single-slide').each(function(index){
+        $(this).css({'width' : slideWidth + '%', 'margin-left' : index * slideWidth + '%'});
+    });
+    
+//     strzałki
+     
+    $('.prev-slide').click(function(){
+        slide(slideIndex - 1);       
+    });
+    
+    $('.next-slide').click(function(){
+        slide(slideIndex + 1);
+    });
+    
+//    funkcja odpowiada za przesuwanie
+    
+    function slide(newSlideIndex){
+        if(newSlideIndex < 0 || newSlideIndex > slideCount - 1)
+            return;
+        
+        var slideCaption = slideShow.find('.slide-caption').eq(newSlideIndex);
+        
+        slideCaption.hide();
+        
+        var newMarginLeft = (newSlideIndex * (-100)) + "%";
+        
+        slideShow.animate({'margin-left' : newMarginLeft}, 800, function(){
+            slideIndex = newSlideIndex;
+            slideCaption.fadeIn();
+        });
+        
+    }
+    
 })
-
 
 
 
