1b647437f80b06498fa590184c332410e3d82886
Mateusz Gołos <golosmateusz@gmail.com>
2018-03-28 19:12:24 +0200

    poprawione, działa

--- zadania-domowe/ajax/js/formularz-ajax.js
+++ zadania-domowe/ajax/js/formularz-ajax.js
@@ -6,9 +6,7 @@ function pobierzDane(event) {
     var pobierz = new XMLHttpRequest();
     pobierz.open("GET", "https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php");
 
-
-
-pobierz.onreadystatechange = function() {
+    pobierz.onreadystatechange = function () {
         if (pobierz.readyState == 4) {
             if (pobierz.status == 200) {
                 var zwrocDane = JSON.parse(pobierz.responseText);
@@ -18,12 +16,9 @@ pobierz.onreadystatechange = function() {
                 pobierz = null;
             }
         }
-    
-    pobierz.send();
-    
     }
+    pobierz.send();
 }
-
 
 
 /*
@@ -31,43 +26,37 @@ wyświetlanie
 */
 
 function dodajDaneDoHtml(dane) {
-
-    // stworzenie DIV
+    // stworzenie DIV - do niego się odnosisz, gdy dodajesz nowe elementy na końcu funkcji
     var divDaneProgramisty = document.createElement("div");
 
     document.body.appendChild(divDaneProgramisty);
-
+    
     // paragrafy z danymi w div
-
     var parDoc1 = document.createElement("p");
     var parImie = document.createElement("p");
     var parNazwisko = document.createElement("p");
     var parZawod = document.createElement("p");
     var parFirma = document.createElement("p");
     var parDoc2 = document.createElement("p");
-    
+
     var parDoc1Content = document.createTextNode("----------");
     var parImieContent = document.createTextNode("Imie: " + dane.imie);
     var parNazwiskoContent = document.createTextNode("Nazwisko: " + dane.nazwisko);
     var parZawodContent = document.createTextNode("Zawód: " + dane.zawod);
     var parFirmaContent = document.createTextNode("Firma: " + dane.firma);
-    var parDoc1Content = document.createTextNode("----------");
+    var parDoc2Content = document.createTextNode("----------");
 
     parDoc1.appendChild(parDoc1Content);
     parImie.appendChild(parImieContent);
-
     parNazwisko.appendChild(parNazwiskoContent);
     parZawod.appendChild(parZawodContent);
     parFirma.appendChild(parFirmaContent);
     parDoc2.appendChild(parDoc2Content);
-    
-    document.div.appendChild(parDoc1);
-    document.div.appendChild(parImie);
-    document.div.appendChild(parNazwisko);
-    document.div.appendChild(parZawod);
-    document.div.appendChild(parFirma);
-    document.div.appendChild(parDoc2);
 
-
-
+    divDaneProgramisty.appendChild(parDoc1);
+    divDaneProgramisty.appendChild(parImie);
+    divDaneProgramisty.appendChild(parNazwisko);
+    divDaneProgramisty.appendChild(parZawod);
+    divDaneProgramisty.appendChild(parFirma);
+    divDaneProgramisty.appendChild(parDoc2);
 }
