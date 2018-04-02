9260d764e1c239c70867be9370814b02165e35e6
Mateusz Gołos <golosmateusz@gmail.com>
2018-04-02 19:15:38 +0200

    Unexpected end of input

--- warsztaty/ajax/warsztat/js/scroll.js
+++ warsztaty/ajax/warsztat/js/scroll.js
@@ -1,20 +1,20 @@
 
 window.onscroll = function(event) {
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-    ajax("GET", "https://jsonplaceholder.typicode.com/users", addToHtml)
+    ajax("GET", "https://jsonplaceholder.typicode.com/users", addToHtml);
     }
 
 
 function addToHtml(data) {
-    for(var i = 0; i < dane.lenght; i++){    
+    for(var i = 0; i < data.lenght; i++){    
     var pUserId = document.createElement("p");
     var pUserName = document.createElement("p");
     var pUserUrl = document.createElement("p");
     var pDoc = document.createElement("p");
     
-    var pUserIdContent = document.createTextNode("User ID: " + dane[i].id);
-    var pUserNameContent = document.createTextNode("User name: " + dane[i].name);
-    var pUserUrlContent = document.createTextNode("User URL: " + dane[i].website);
+    var pUserIdContent = document.createTextNode("User ID: " + data[i].id);
+    var pUserNameContent = document.createTextNode("User name: " + data[i].name);
+    var pUserUrlContent = document.createTextNode("User URL: " + data[i].website);
     var pDocContent = document.createTextNode("-------------------");
     
     pUserId.appendChild(pUserIdContent);
@@ -33,9 +33,7 @@ function addToHtml(data) {
 
 function ajax(method, url, callback){
     var httpRequest = new XMLHttpRequest();
-    
     httpRequest.open(method, url);
-    
     httpRequest.onreadystatechange = function(){
         if(httpRequest.readyState == 4){
             if(httpRequest.status == 200){
@@ -47,6 +45,6 @@ function ajax(method, url, callback){
             }
         }
     }
-}
     httpRequest.send();
+}
 
