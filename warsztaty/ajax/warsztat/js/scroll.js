e804f98ec327e62ac712e255432bbaaaad3a5f8d
Mateusz Gołos <golosmateusz@gmail.com>
2018-04-07 09:06:30 +0200

    .

--- warsztaty/ajax/warsztat/js/scroll.js
+++ warsztaty/ajax/warsztat/js/scroll.js
@@ -1,52 +1,51 @@
+window.onscroll = function (event) {
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+            ajax("GET", "https://jsonplaceholder.typicode.com/users", addToHtml)
+        }
 
-window.onscroll = function(event) {
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-    ajax("GET", "https://jsonplaceholder.typicode.com/users", addToHtml)
-    }
 
+        function addToHtml(data) {
+            for (var i = 0; i < data.lenght; i++) {
+                var pUserId = document.createElement("p");
+                var pUserName = document.createElement("p");
+                var pUserUrl = document.createElement("p");
+                var pDoc = document.createElement("p");
 
-function addToHtml(data) {
-    for(var i = 0; i < data.lenght; i++){    
-    var pUserId = document.createElement("p");
-    var pUserName = document.createElement("p");
-    var pUserUrl = document.createElement("p");
-    var pDoc = document.createElement("p");
-    
-    var pUserIdContent = document.createTextNode("User ID: " + data[i].id);
-    var pUserNameContent = document.createTextNode("User name: " + data[i].name);
-    var pUserUrlContent = document.createTextNode("User URL: " + data[i].website);
-    var pDocContent = document.createTextNode("-------------------");
-    
-    pUserId.appendChild(pUserIdContent);
-    pUserName.appendChild(pUserNameContent);
-    pUserUrl.appendChild(pUserUrlContent);
-    pDoc.appendChild(pDocContent);
-    
-    document.body.appendChild(pUserId);
-    document.body.appendChild(pUserName);
-    document.body.appendChild(pUserUrl);
-    document.body.appendChild(pDoc);
-}
-}
+                var pUserIdContent = document.createTextNode("User ID: " + data[i].id);
+                var pUserNameContent = document.createTextNode("User name: " + data[i].name);
+                var pUserUrlContent = document.createTextNode("User URL: " + data[i].website);
+                var pDocContent = document.createTextNode("-------------------");
 
-// definicja funkcji ajax
+                pUserId.appendChild(pUserIdContent);
+                pUserName.appendChild(pUserNameContent);
+                pUserUrl.appendChild(pUserUrlContent);
+                pDoc.appendChild(pDocContent);
 
-function ajax(method, url, callback){
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.open(method, url);
-    httpRequest.onreadystatechange = function(){
-        if(httpRequest.readyState == 4){
-            if(httpRequest.status == 200){
-                var returnData = httpRequest.responseText;
-                
-                var jsonObj = JSON.parse(returnData);
-                
-                callback(jsonObj);
-                
-                httpRequest = null;
+                document.body.appendChild(pUserId);
+                document.body.appendChild(pUserName);
+                document.body.appendChild(pUserUrl);
+                document.body.appendChild(pDoc);
             }
         }
-    }
-    httpRequest.send();
-}
 
+        // definicja funkcji ajax
+
+        function ajax(method, url, callback) {
+            var httpRequest = new XMLHttpRequest();
+
+            httpRequest.open(method, url);
+
+            httpRequest.onreadystatechange = function () {
+                if (httpRequest.readyState == 4) {
+                    if (httpRequest.status == 200) {
+                        var returnData = httpRequest.responseText;
+
+                        callback(returnData);
+
+                        httpRequest = null;
+                    }
+                }
+            }
+            httpRequest.send();
+        }
+}
