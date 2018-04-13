f9f703bec1f735f84899f39eeef4b3b5e5aac722
Mateusz Gołos <golosmateusz@gmail.com>
2018-04-13 06:24:29 +0200

    Poprawione

---
+++ moje-portfolio/js/main.js
@@ -0,0 +1,9 @@
+// button
+
+$("a[href^='#']").on("click", function(event) {
+    event.preventDefault();
+    $('html, body').animate(
+    { scrollTop: $(this.hash).offset().top},
+    400
+    );
+})
