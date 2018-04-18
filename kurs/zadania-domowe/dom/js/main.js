a16b0f488e9ccdacf9a5a814170d24ac92983bba
Mateogos22 <golosmateusz@gmail.com>
2018-04-18 06:56:10 +0200

    .

--- kurs/zadania-domowe/dom/js/main.js
+++ kurs/zadania-domowe/dom/js/main.js
@@ -1,7 +1,7 @@
 $('#count-sum').click(function(){
     var sum = 0;
     $('.salary').each(function(){
-        sum += parseInt(this.innerHTML, 10)
+        sum += parseInt(this.innerHTML)
     })
     
     $('#sum').text(sum);
