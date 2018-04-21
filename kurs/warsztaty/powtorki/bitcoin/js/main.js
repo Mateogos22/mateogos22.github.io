f4b5229a6a2b38d55c34fa1317b88c716055b808
Mateusz Gołos <golosmateusz@gmail.com>
2018-04-21 16:35:10 +0200

    .

---
+++ kurs/warsztaty/powtorki/bitcoin/js/main.js
@@ -0,0 +1,43 @@
+
+
+$(document).ready(function() {
+   
+    function pobierzKursBitcoin( url ) {
+        
+        
+        $.getJSON( url, function( data ) {
+            
+        var currentBuyPrice = parseFloat( $('#buy').html() );
+        var currentSellPrice = parseFloat( $('#sell').html() );
+        var buyArrow = $('#buy-arrow');
+        var sellArrow = $('#sell-arrow');
+                    
+            $('#buy').html(data.PLN.buy);
+            $('#sell').html(data.PLN.sell);
+            
+            if( currentBuyPrice > parseFloat(data.PLN.buy) ) {
+                buyArrow.css('color', 'red').removeAttr('class').addClass('fas fa-arrow-down');
+            } else if( currentBuyPricePricePrice < parseFloat(data.PLN.buy) ) {
+                sellArrow.css('color', 'green').removeAttr('class').addClass('fas fa-arrow-up');                
+            } else {
+                sellArrow.css('color', 'black').removeAttr('class').addClass('fas fa-minus-square');                
+            } 
+            
+            if( currentSellPrice > parseFloat(data.PLN.sell) ) {
+                buyArrow.css('color', 'red').removeAttr('class').addClass('fas fa-arrow-down');
+            } else if( currentSellPricePrice < parseFloat(data.PLN.sell) ) {
+                sellArrow.css('color', 'green').removeAttr('class').addClass('fas fa-arrow-up');                
+            } else {
+                sellArrow.css('color', 'black').removeAttr('class').addClass('fas fa-minus-square');                
+            }
+        });
+    
+    pobierzKursBitcoin( 'https://blockchain.info/pl/ticker' );
+    
+    
+    $('.control-button').on('click', function() {
+    pobierzKursBitcoin( 'https://blockchain.info/pl/ticker' );        
+    });
+   
+} );
+    
--- kurs/zadania-domowe/bootstrap/app/js/main.js
+++
@@ -1,9 +0,0 @@
-// button
-
-$("a[href^='#']").on("click", function(event) {
-    event.preventDefault();
-    $('html, body').animate(
-    { scrollTop: $(this.hash).offset().top},
-    400
-    );
-})
